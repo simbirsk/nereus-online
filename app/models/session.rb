277bc6efb68a82e8ba0480cc065e49c1f884e387
Simbirsk <albertegz@gmail.com>
2013-05-12 12:14:25 +0200

    session user accessible

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,5 @@
 class Session < ActiveRecord::Base
   belongs_to :weather
   belongs_to :user
-  attr_accessible :date, :pressure, :swell, :temperature, :wind_direction, :wind_force
+  attr_accessible :date, :pressure, :swell, :temperature, :wind_direction, :wind_force, :user
 end
