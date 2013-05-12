6896fd8ea0cf48b3c6cae82bac3ac91fcec119ad
Simbirsk <albertegz@gmail.com>
2013-05-12 14:22:09 +0200

    session data allows blank

--- app/models/session.rb
+++ app/models/session.rb
@@ -4,9 +4,9 @@ class Session < ActiveRecord::Base
 
   attr_accessible :date, :pressure, :swell, :temperature, :wind_direction, :wind_force, :user
 
-  validates :wind_force, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 12}
-  validates :wind_direction, numericality: {greater_than_or_equal_to: 0, less_than: 360}
-  validates :temperature, numericality: {greater_than_or_equal_to: -50, less_than_or_equal_to: 50}
-  validates :pressure, numericality: {greater_than_or_equal_to: 870, less_than_or_equal_to: 1085}
-  validates :swell, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 16}
+  validates :wind_force, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 12}, :allow_blank => true
+  validates :wind_direction, numericality: {greater_than_or_equal_to: 0, less_than: 360}, :allow_blank => true
+  validates :temperature, numericality: {greater_than_or_equal_to: -50, less_than_or_equal_to: 50}, :allow_blank => true
+  validates :pressure, numericality: {greater_than_or_equal_to: 870, less_than_or_equal_to: 1085}, :allow_blank => true
+  validates :swell, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 16}, :allow_blank => true
 end
