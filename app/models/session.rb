ec356b62938329fa10f747755a4c81fead7ff20e
Simbirsk <albertegz@gmail.com>
2013-05-12 12:58:49 +0200

    validated session parameters

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,12 @@
 class Session < ActiveRecord::Base
   belongs_to :weather
   belongs_to :user
+
+  validates :wind_force, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 12}
+  validates :wind_direction, numericality: {greater_than_or_equal_to: 0, less_than: 360}
+  validates :temperature, numericality: {greater_than_or_equal_to: -50, less_than_or_equal_to: 50}
+  validates :pressure, numericality: {greater_than_or_equal_to: 870, less_than_or_equal_to: 1085}
+  validates :swell, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 16}
+
   attr_accessible :date, :pressure, :swell, :temperature, :wind_direction, :wind_force, :user
 end
