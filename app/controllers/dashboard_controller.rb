0e488602b5e2d38e5d32818ca83f436c50cbe6c7
albertedevigo <albertegz@gmail.com>
2013-05-14 11:09:14 +0200

    added fishing_journey table to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
   def index
+    @fishing_journeys = FishingJourney.accessible_by(current_ability)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @fishing_journeys }
+    end
   end
 end
