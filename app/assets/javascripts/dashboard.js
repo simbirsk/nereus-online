66277417c2cdd44bbd292afd28c8346480b903c2
albertedevigo <albertegz@gmail.com>
2013-05-16 10:27:24 +0200

    chart.js working

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -21,4 +21,4 @@ var data = {
 //Get context with jQuery - using jQuery's .get() method.
 var ctx = $("#myChart").get(0).getContext("2d");
 //This will get the first returned node in the jQuery collection.
-var myNewChart = new Chart(ctx).Line(data,options);
+var myNewChart = new Chart(ctx).Line(data);
