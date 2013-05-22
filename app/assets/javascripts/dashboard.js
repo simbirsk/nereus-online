4eb5a8b0da6fd10ec65120ef79c914024ecbf327
albertedevigo <albertegz@gmail.com>
2013-05-22 19:15:27 +0200

    moved back...

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,35 +1,24 @@
-Gmaps.map.callback = function() {
-    if (Gmaps.map.markers.length <= 1) {
-        setTimeout(function() {
-            Gmaps.map.serviceObject.setZoom(2)
-        }, 50)
-    } else {
-        Gmaps.map.map_options.auto_zoom = true
-        Gmaps.map.adjustMapToBounds()
-    }
-}
-
 var data = {
     labels : ["January","February","March","April","May","June","July"],
     datasets : [
         {
-            fillColor : "rgba(220,220,220,0.5)",
-            strokeColor : "rgba(220,220,220,1)",
-            pointColor : "rgba(220,220,220,1)",
+            fillColor        : "rgba(220,220,220,0.5)",
+            strokeColor      : "rgba(220,220,220,1)",
+            pointColor       : "rgba(220,220,220,1)",
             pointStrokeColor : "#fff",
-            data : [65,59,90,81,56,55,40]
+            data             : [65,59,90,81,56,55,40]
         },
         {
-            fillColor : "rgba(151,187,205,0.5)",
-            strokeColor : "rgba(151,187,205,1)",
-            pointColor : "rgba(151,187,205,1)",
+            fillColor        : "rgba(151,187,205,0.5)",
+            strokeColor      : "rgba(151,187,205,1)",
+            pointColor       : "rgba(151,187,205,1)",
             pointStrokeColor : "#fff",
-            data : [28,48,40,19,96,27,100]
+            data             : [28,48,40,19,96,27,100]
         }
     ]
 }
 
 //Get context with jQuery - using jQuery's .get() method.
-var ctx = $("#myChart").get(0).getContext("2d");
+var ctx = $("#myChart").get(0).getContext("2d")
 //This will get the first returned node in the jQuery collection.
-var myNewChart = new Chart(ctx).Line(data);
+var myNewChart = new Chart(ctx).Line(data)
